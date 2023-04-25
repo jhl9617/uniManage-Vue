9fcfcdc8bd25913b1b66420605968d09cf688dc0
JonghaLee <jhl9617@gmail.com>
2023-04-25 14:44:37 +0900

    run serve port 3434 run build port 9090으로 변경 파일 추가

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/images/logo.png">
+  <HelloWorld msg="Vue : samplevue and Boot : binevue 연동"/> -->
+
+  <PageHeader/> <!--헤더 컴포넌트-->
+
+  <router-view/> <!--페이지 이동이 표시될 영역을 의미함-->
+
+  <PageFooter/> <!--푸터 컴포넌트-->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import PageHeader from './components/common/PageHeader.vue';
+import PageFooter from './components/common/PageFooter.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    PageHeader,
+    PageFooter
   }
 }
 </script>
 
 <style>
 #app {
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
