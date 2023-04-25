231edae585019603d0b8ab785f766752b4ddfbce
JonghaLee <jhl9617@gmail.com>
2023-04-25 18:17:30 +0900

    adminHeader 임시추가

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,25 @@
   <HelloWorld msg="Vue : samplevue and Boot : binevue 연동"/> -->
 
   <PageHeader/> <!--헤더 컴포넌트-->
-
+  <AdminHeader/>
   <router-view/> <!--페이지 이동이 표시될 영역을 의미함-->
 
   <PageFooter/> <!--푸터 컴포넌트-->
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+
 import PageHeader from './components/common/PageHeader.vue';
 import PageFooter from './components/common/PageFooter.vue';
+import AdminHeader from './views/admin/common/AdminHeader.vue';
+
 
 export default {
   name: 'App',
   components: {
     // HelloWorld
     PageHeader,
+      AdminHeader,
     PageFooter
   }
 }
