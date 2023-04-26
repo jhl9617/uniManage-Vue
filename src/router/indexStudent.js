24821b81909d0d407b0ac01b78b81f9b96ed1b70
nulljump <121776248+solra5653@users.noreply.github.com>
2023-04-26 17:17:02 +0900

    2023-04-26 프론트 작업
    EclassMain, LectureMain, StudentMain . vue 생성
    
    indexStudent.js 수정

--- src/router/indexStudent.js
+++ src/router/indexStudent.js
@@ -1,93 +1,145 @@
-import PageHome from "@/views/student/Eclass/PageHome.vue";
-import BoardList from '@/views/student/Eclass/BoardList.vue'
-import BoardDetail from '@/views/student/Eclass/BoardDetail.vue'
-import BoardWrite from '@/views/student/Eclass/BoardWrite.vue'
-import SourceWrite from "@/views/student/Eclass/SourceWrite.vue";
-import SourceList from "@/views/student/Eclass/SourceList.vue";
-import SourceDetail from "@/views/student/Eclass/SourceDetail.vue";
-import NoticeList from "@/views/student/Eclass/NoticeList.vue";
-import NoticeDetail from "@/views/student/Eclass/NoticeDetail.vue";
-import HomeworkList from "@/views/student/Eclass/HomeworkList.vue";
-import HomeworkWrite from "@/views/student/Eclass/HomeworkWrite.vue";
-import HomeworkDetail from "@/views/student/Eclass/HomeworkDetail.vue";
-import LectureInfo from "@/views/student/Eclass/LectureInfo.vue";
-
+import EclassLectureMain from "@/views/student/Eclass/LectureMain.vue";
+import EclassBoardList from '@/views/student/Eclass/BoardList.vue'
+import EclassBoardDetail from '@/views/student/Eclass/BoardDetail.vue'
+import EclassBoardWrite from '@/views/student/Eclass/BoardWrite.vue'
+import EclassSourceList from "@/views/student/Eclass/SourceList.vue";
+import EclassSourceDetail from "@/views/student/Eclass/SourceDetail.vue";
+import EclassNoticeList from "@/views/student/Eclass/NoticeList.vue";
+import EclassNoticeDetail from "@/views/student/Eclass/NoticeDetail.vue";
+import EclassHomeworkList from "@/views/student/Eclass/HomeworkList.vue";
+import EclassHomeworkWrite from "@/views/student/Eclass/HomeworkWrite.vue";
+import EclassHomeworkDetail from "@/views/student/Eclass/HomeworkDetail.vue";
+import EclassLectureInfo from "@/views/student/Eclass/LectureInfo.vue";
+import EclassMain from "@/views/student/Eclass/EclassMain.vue";
+import StudentStatus from "@/views/student/studentInfoSystem/StudentStatus.vue";
+import ApplyTakeOff from "@/views/student/studentInfoSystem/ApplyTakeOff.vue";
+import ApplyReturn from "@/views/student/studentInfoSystem/ApplyReturn.vue";
+import TimelineByDepart from "@/views/student/studentInfoSystem/TimelineByDepart.vue";
+import StudentTimetable from "@/views/student/studentInfoSystem/StudentTimetable.vue";
+import CheckCourse from "@/views/student/studentInfoSystem/CheckCourse.vue";
+import PayReceipt from "@/views/student/studentInfoSystem/PayReceipt.vue";
+import PrintReceipt from "@/views/student/studentInfoSystem/PrintReceipt.vue";
+import SurveyCourse from "@/views/student/studentInfoSystem/SurveyCourse.vue";
+import StudentMain from "@/views/student/StudentMain.vue";
 const routesStudent = [
-    {
-        path: '/',
-        name: 'PageHome',
-        component: PageHome
-    },
-    {
-        path: '/about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '@/views/student/Eclass/PageAbout.vue')
 
+    {
+        path: '/student',
+        name: 'StudentMain',
+        component: StudentMain
     },
     {
-        path: '/board/list',
-        name: 'BoardList',
-        component: BoardList
+        path: '/eclass',
+        name: 'EclassMain',
+        component: EclassMain
     },
     {
-        path: '/board/detail',
-        name: 'BoardDetail',
-        component: BoardDetail
-    },
-    {
-        path: '/board/write',
-        name: 'BoardWrite',
-        component: BoardWrite
+        path: '/eclass/1',
+        name: 'EclassLectureMain',
+        component: EclassLectureMain
     },
 
     {
-        path: '/source/write',
-        name: 'SourceWrite',
-        component: SourceWrite
+        path: '/eclass/board/list',
+        name: 'EclassBoardList',
+        component: EclassBoardList
     },
     {
-        path: '/source/list',
-        name: 'SourceList',
-        component: SourceList
+        path: '/eclass/board/detail',
+        name: 'EclassBoardDetail',
+        component: EclassBoardDetail
     },
     {
-        path: '/source/detail',
-        name: 'SourceDetail',
-        component: SourceDetail
+        path: '/eclass/board/write',
+        name: 'EclassBoardWrite',
+        component: EclassBoardWrite
     },
     {
-        path: '/notice/list',
-        name: 'NoticeList',
-        component: NoticeList
+        path: '/eclass/source/list',
+        name: 'EclassSourceList',
+        component: EclassSourceList
     },
     {
-        path: '/notice/detail',
-        name: 'NoticeDetail',
-        component: NoticeDetail
+        path: '/eclass/source/detail',
+        name: 'EclassSourceDetail',
+        component: EclassSourceDetail
     },
     {
-        path: '/homework/list',
-        name: 'HomeworkList',
-        component: HomeworkList
+        path: '/eclass/notice/list',
+        name: 'EclassNoticeList',
+        component: EclassNoticeList
     },
     {
-        path: '/homework/detail',
-        name: 'HomeworkDetail',
-        component: HomeworkDetail
+        path: '/eclass/notice/detail',
+        name: 'EclassNoticeDetail',
+        component: EclassNoticeDetail
     },
     {
-        path: '/homework/write',
-        name: 'HomeworkWrite',
-        component: HomeworkWrite
+        path: '/eclass/homework/list',
+        name: 'EclassHomeworkList',
+        component: EclassHomeworkList
     },
     {
-        path: '/lectureinfo',
-        name: 'LectureInfo',
-        component: LectureInfo
+        path: '/eclass/homework/detail',
+        name: 'EclassHomeworkDetail',
+        component: EclassHomeworkDetail
     },
+    {
+        path: '/eclass/homework/write',
+        name: 'EclassHomeworkWrite',
+        component: EclassHomeworkWrite
+    },
+    {
+        path: '/eclass/lectureinfo',
+        name: 'EclassLectureInfo',
+        component: EclassLectureInfo
+
+    },
+    {
+        path: '/student/status',
+        name: 'StudentStatus',
+        component: StudentStatus
+    },
+    {
+        path: '/student/takeoff',
+        name: 'ApplyTakeOff',
+        component: ApplyTakeOff
+    },
+    {
+        path: '/student/return',
+        name: 'ApplyReturn',
+        component: ApplyReturn
+    },
+    {
+        path: '/student/timelinebydepart',
+        name: 'TimelineByDepart',
+        component: TimelineByDepart
+    },
+    {
+        path: '/student/studenttimetable',
+        name: 'StudentTimetable',
+        component: StudentTimetable
+    },
+    {
+        path: '/student/checkcourse',
+        name: 'CheckCourse',
+        component: CheckCourse
+    },
+    {
+        path: '/student/payreceipt',
+        name: 'PayReceipt',
+        component: PayReceipt
+    },
+    {
+        path: '/student/printreceipt',
+        name: 'PrintReceipt',
+        component: PrintReceipt
+    },
+    {
+        path: '/student/surveycourse',
+        name: 'SurveyCourse',
+        component: SurveyCourse
+    }
 ]
 
 export default routesStudent
---
+++ src/views/student/StudentMain.vue
@@ -0,0 +1,126 @@
+<template>
+    <div class="container">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+            <a class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none" href="/">
+                <svg class="bi me-2" height="32" width="40">
+                    <use xlink:href="#bootstrap"></use>
+                </svg>
+                <span class="fs-4"> OO 대학교</span>
+            </a>
+
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a aria-current="page" class="nav-link" href="#">학교홈페이지</a></li>
+                <li class="nav-item">
+                    <router-link class="nav-link active" to="#">학생정보시스템</router-link>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="#">OFFICE 365</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">도서관</a></li>
+                <li class="nav-item">
+                    <router-link class="nav-link active" to="/eclass">E-CLASS</router-link>
+                </li>
+            </ul>
+        </header>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="col">
+                    <div class="d-flex flex-column align-items-center text-center">
+                        <img alt="Admin" class="rounded-circle" src="https://bootdey.com/img/Content/avatar/avatar7.png"
+                             width="150">
+                        <div class="mt-3">
+                            <h4>OOO 학생</h4>
+                            <p class="text-secondary mb-1"> OO 학과</p>
+                            <button class="btn btn-outline-primary me-2" type="button">LogOut</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="row">
+                    <div class="col-md-8">
+                        수강신청
+                        <br><br>
+                        <div class="container">
+                            <table align="center" class="w3-table-all table-bordered" width="505">
+                                <tr align="left">
+                                    <th>검색구분</th>
+                                    <td>
+                                        <select>
+                                            <option value="">- 선택 -</option>
+                                            <option value="">학과</option>
+                                        </select>
+                                    </td>
+                                    <th>학과</th>
+                                    <td>
+                                        <select>
+                                            <option value="">- 선택 -</option>
+                                            <option value="">경영학과</option>
+                                            <option value="">컴퓨터공학과</option>
+                                            <option value="">잔자공학과</option>
+                                            <option value="">중어중문학과</option>
+                                            <option value="">영어영문학과</option>
+                                        </select>
+                                    </td>
+                                </tr>
+                            </table>
+                            <table class="w3-table-all">
+                            <tr>
+                                <th>강의명</th>
+                                <th>학점</th>
+                                <th>강의시간</th>
+                                <th>수강신청</th>
+                            </tr>
+                                <tr>
+                                    <td>강의명</td>
+                                    <td>학점</td>
+                                    <td>강의시간</td>
+                                    <td><button class="btn btn-outline-dark" type="button">수강신청</button></td>
+                                </tr>
+                                <tr>
+                                    <td>강의명</td>
+                                    <td>학점</td>
+                                    <td>강의시간</td>
+                                    <td><button class="btn btn-outline-dark" type="button">수강신청</button></td>
+                                </tr>
+                                <tr>
+                                    <td>강의명</td>
+                                    <td>학점</td>
+                                    <td>강의시간</td>
+                                    <td><button class="btn btn-outline-dark" type="button">수강신청</button></td>
+                                </tr>
+
+                            </table>
+                        </div>
+                    </div>
+                    <div class=" col-md-4
+                            ">
+                            챗봇
+                            <br><br>
+                            <div class="container">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <PageFooter/>
+        </div>
+</template>
+
+<script>
+import PageFooter from "@/components/common/PageFooter.vue";
+
+
+export default {
+    components: {
+        // HelloWorld
+        PageFooter,
+
+    }
+}
+</script>
+<style scoped>
+</style>
