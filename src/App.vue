0a646c409a621325f8fac914cc886a480a7c8545
bidrihi <bidrihi@gmail.com>
2023-04-26 10:49:28 +0900

    교수 개인정보 페이지 작성중

--- src/App.vue
+++ src/App.vue
@@ -10,39 +10,34 @@
                     <router-view/>
                 </div>
             </div>
-
         </div>
         <PageFooter/>
     </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 import PageHeader from './components/common/PageHeader.vue';
 import PageFooter from './components/common/PageFooter.vue';
 import PageSidebar from "@/views/student/studentInfoSystem/PageSidebar.vue";
 
 export default {
-  name: 'App',
-  components: {
-
-    // HelloWorld
-    PageHeader,
-    PageFooter,
-      PageSidebar
-  }
-
+    name: 'App',
+    components: {
+        PageHeader,
+        PageFooter,
+        PageSidebar
+    }
 }
 </script>
 
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- src/views/professor/PersonInfo.vue
+++ src/views/professor/PersonInfo.vue
@@ -1,9 +1,43 @@
 <!-- PersonInfo.vue -->
 <template>
-  <div>
-
-  </div>
+    <div class="d-flex justify-content-center">
+        <div>
+            개인정보
+            <div>
+                <img src="" alt="">
+                소속 :
+                구분 :
+            </div>
+            <button value="비밀번호변경"/>
+        </div>
+        <div>
+            교수 공지사항
+            <table>
+                <tr>
+                    <th>제목</th>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <div class="d-flex justify-content-center">
+        <div>
+            <table>
+                <tr>
+                    <th></th>
+                    <th>월</th>
+                    <th>화</th>
+                    <th>수</th>
+                    <th>목</th>
+                    <th>금</th>
+                </tr>
+            </table>
+        </div>
+        <div>
+            학사일정
+        </div>
+    </div>
 </template>
 <script>
 </script>
-<style></style>
+<style>
+</style>
