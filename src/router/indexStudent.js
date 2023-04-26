98a34422f23c94463e606da101acd8f45a3c4a7d
moharago <yuna922@naver.com>
2023-04-26 11:35:25 +0900

    학생정보시스템 indexStudent 추가

--- src/router/indexStudent.js
+++ src/router/indexStudent.js
@@ -11,6 +11,16 @@ import HomeworkList from "@/views/student/Eclass/HomeworkList.vue";
 import HomeworkWrite from "@/views/student/Eclass/HomeworkWrite.vue";
 import HomeworkDetail from "@/views/student/Eclass/HomeworkDetail.vue";
 import LectureInfo from "@/views/student/Eclass/LectureInfo.vue";
+import StudentStatus from "@/views/student/studentInfoSystem/StudentStatus.vue";
+import ApplyTakeOff from "@/views/student/studentInfoSystem/ApplyTakeOff.vue";
+import ApplyReturn from "@/views/student/studentInfoSystem/ApplyReturn.vue";
+import TimelineByDepart from "@/views/student/studentInfoSystem/TimelineByDepart.vue";
+import StudentTimetable from "@/views/student/studentInfoSystem/StudentTimetable.vue";
+import CheckCourse from "@/views/student/studentInfoSystem/CheckCourse.vue";
+import PayReceipt from "@/views/student/studentInfoSystem/PayReceipt.vue";
+import PrintReceipt from "@/views/student/studentInfoSystem/PrintReceipt.vue";
+import SurveyCourse from "@/views/student/studentInfoSystem/SurveyCourse.vue";
+
 
 const routesStudent = [
     {
@@ -87,7 +97,51 @@ const routesStudent = [
         path: '/lectureinfo',
         name: 'LectureInfo',
         component: LectureInfo
+    },{
+        path: '/student/status',
+        name: 'StudentStatus',
+        component: StudentStatus
     },
+    {
+        path: '/student/takeoff',
+        name: 'ApplyTakeOff',
+        component: ApplyTakeOff
+    },
+    {
+        path: '/student/return',
+        name: 'ApplyReturn',
+        component: ApplyReturn
+    },
+    {
+        path: '/student/timelinebydepart',
+        name: 'TimelineByDepart',
+        component: TimelineByDepart
+    },
+    {
+        path: '/student/studenttimetable',
+        name: 'StudentTimetable',
+        component: StudentTimetable
+    },
+    {
+        path: '/student/checkcourse',
+        name: 'CheckCourse',
+        component: CheckCourse
+    },
+    {
+        path: '/student/payreceipt',
+        name: 'PayReceipt',
+        component: PayReceipt
+    },
+    {
+        path: '/student/printreceipt',
+        name: 'PrintReceipt',
+        component: PrintReceipt
+    },
+    {
+        path: '/student/surveycourse',
+        name: 'SurveyCourse',
+        component: SurveyCourse
+    }
 ]
 
 export default routesStudent
