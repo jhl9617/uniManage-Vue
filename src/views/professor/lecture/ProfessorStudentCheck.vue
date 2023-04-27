49b6fcfa192891bf8f20921d41de8be183d4ba88
su jin <itsyksj@gmail.com>
2023-04-27 10:54:41 +0900

    교수 페이지 연결

---
+++ src/views/professor/lecture/ProfessorStudentCheck.vue
@@ -0,0 +1,13 @@
+<template>
+  학생 출결관리
+</template>
+
+<script>
+export default {
+    name: "ProfessorStudentCheck.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/professor/lectureEvaluation/ProfessorLectureEvaluation.vue
@@ -0,0 +1,13 @@
+<template>
+  강의 평가내역
+</template>
+
+<script>
+export default {
+    name: "ProfessorLectureEvaluation.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
