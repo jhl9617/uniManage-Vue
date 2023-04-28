232cd24cd448011517d565ee5cf91016e6b644ab
JonghaLee <jhl9617@gmail.com>
2023-04-28 09:51:41 +0900

    PageHeader.vue 수정

--- src/views/admin/manage/AddStudent.vue
+++ src/views/admin/manage/AddStudent.vue
@@ -1,57 +1,100 @@
 <template>
     학생 추가
-    <form>
-        <div class="form-group">
-            <div class="col-md-4">
-                <label for="exampleFormControlInput1">이름</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="학생 이름">
-            </div>
-            <div class="col-md-8">
-                <form method="post" enctype="multipart/form-data">
-                    <div class="button">
-                        <label for="chooseFile">
-                            <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
-                        </label>
+    <div style="border:2px solid black; padding:100px;">
+        <div class="col-md-8">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="d-flex flex-column align-items-center text-center">
+                            <fieldset>
+                                <table>
+                                    <td>
+                                        <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
+                                    </td>
+                                    <tr>
+                                        <td>
+                                            <button class="btn btn-primary">이미지 업로드</button>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </fieldset>
+                        </div>
                     </div>
-                    <input type="file" id="chooseFile" name="chooseFile" accept="image/*" onchange="loadFile(this)">
-                </form>
+                    <div class="col-md-8">
+                        <fieldset>
+                            <table class="w3-table-all">
+                                <tr>
+                                    <td>이름</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>학번(ID)</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>생년월일</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>소속</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>우편번호</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>주소</td>
+                                    <td>
+                                        <input type="text" id="sample6_postcode" placeholder="우편번호">
+                                        <input type="button" onclick="sample6_execDaumPostcode()" value="우편번호 찾기"><br>
+                                        <input type="text" id="sample6_address" placeholder="주소"><br>
+                                        <input type="text" id="sample6_detailAddress" placeholder="상세주소">
+                                        <input type="text" id="sample6_extraAddress" placeholder="참고항목">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>성별</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>휴대전화번호</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>e-mail</td>
+                                    <td></td>
+                                </tr>
+                            </table>
+                        </fieldset>
+                    </div>
+                </div>
+                <br>
+
             </div>
-
+            <div align="center">
+                <button type="button">추가</button>&nbsp;&nbsp;
+                <button type="button">목록</button>
+            </div>
         </div>
-        <br><br>
-        <div class="form-group">
-            <label for="exampleFormControlInput1">이메일</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-        </div>
+    </div>
+</template>
 
-        <div class="form-group">
-            <label for="exampleFormControlSelect1">학과</label>
+<script>
+export default {
+    name: "AddProfessor"
+}
+</script>
+
+<style scoped>
+
+</style>
+
+<!--            <label for="exampleFormControlSelect1">학과</label>
             <select class="form-control" id="exampleFormControlSelect1">
                 <option>경영학과</option>
                 <option>컴퓨터공학과</option>
                 <option>전자공학과</option>
                 <option>중어중문학과</option>
                 <option>영어영문학과</option>
-            </select>
-        </div>
-
-        <div class="form-group">
-            <label for="exampleFormControlInput1">생년월일(6자리)</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="생년월일(6자리)">
-        </div>
-        <div class="form-group">
-            <label for="exampleFormControlInput1">주소</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="주소">
-        </div>
-        <div class="form-group">
-            <label for="exampleFormControlInput1">상세주소</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="상세주소">
-        </div>
-        <div class="form-group">
-            <label for="exampleFormControlInput1">우편번호</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="우편번호">
-        </div>
-    </form>
-</template>
-<script>
-</script>
+            </select>-->
