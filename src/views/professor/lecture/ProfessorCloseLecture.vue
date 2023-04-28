4e73f9a72ec15875f927712a0ba64321d1c029d7
bidrihi <bidrihi@gmail.com>
2023-04-28 10:15:03 +0900

    휴강 신청 리스트 폼 추가

--- src/views/professor/lecture/ProfessorCloseLecture.vue
+++ src/views/professor/lecture/ProfessorCloseLecture.vue
@@ -1,5 +1,49 @@
 <template>
-  휴강
+    <div class="container">
+        <div class="row">
+            <h3>휴강신청 내역</h3>
+            <br>
+            <table class="table table-bordered" id="closelist">
+                <thead>
+                <tr>
+                    <th>번호</th>
+                    <th>강의명</th>
+                    <th>휴강일</th>
+                    <th>휴강시간</th>
+                    <th>보강일</th>
+                    <th>보강시간</th>
+                    <th>제출서류</th>
+                    <th>승인여부</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>2</td>
+                    <td>공업미적분</td>
+                    <td>2023.04.28</td>
+                    <td>09:00~11:00</td>
+                    <td>2023.05.03</td>
+                    <td>15:00~17:00</td>
+                    <td></td>
+                    <td>승인대기</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>공업미적분</td>
+                    <td>2023.04.24</td>
+                    <td>14:00~16:00</td>
+                    <td>2023.04.26</td>
+                    <td>09:00~11:00</td>
+                    <td>◎</td>
+                    <td>승인완료</td>
+                </tr>
+                </tbody>
+            </table>
+            <div>
+                <input type="button" value="휴강신청">
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +53,7 @@ export default {
 </script>
 
 <style scoped>
-
+#closelist tr td {
+    text-align: center;
+}
 </style>
