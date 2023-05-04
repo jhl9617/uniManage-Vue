6d5102a90a7a88477972ae0cb049a878d65aa9c0
JonghaLee <jhl9617@gmail.com>
2023-05-04 09:45:24 +0900

    login page에 member_id member_pwd에 값 전송하게 수정

--- src/router/index.js
+++ src/router/index.js
@@ -39,10 +39,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '@/views/student/Eclass/PageAbout.vue')
-
   },
-
-
 ]
 
 const allRoutes = [...routes, ...routesAdmin, ...routesProf, ...routesStudent]
