960e27cdb55bcd5a7c5d11ccae219aa6a8f5645c
moharago <yuna922@naver.com>
2023-05-22 13:09:37 +0900

    학생정보시스템 각 시스템에서 로그인한 멤버 정보 띄우기

--- src/router/indexStudent.js
+++ src/router/indexStudent.js
@@ -15,20 +15,20 @@ import LectureHomeworkDetail from "@/views/student/Eclass/lecture/homework/Homew
 import LectureInfo from "@/views/student/Eclass/lecture/LectureInfo.vue";
 import EclassMain from "@/views/student/Eclass/common/EclassMain.vue";
 
-import StudentStatus from "@/views/student/studentInfoSystem/StudentStatus.vue";
-import StudentApplyTakeOff from "@/views/student/studentInfoSystem/StudentApplyTakeOff.vue";
-import StudentApplyReturn from "@/views/student/studentInfoSystem/StudentApplyReturn.vue";
-import StudentTimelineByDepart from "@/views/student/studentInfoSystem/TimelineByDepart.vue";
-import StudentTimetable from "@/views/student/studentInfoSystem/StudentTimetable.vue";
-import StudentCheckCourse from "@/views/student/studentInfoSystem/CheckCourse.vue";
-import StudentPayReceipt from "@/views/student/studentInfoSystem/PayReceipt.vue";
-import StudentPrintReceipt from "@/views/student/studentInfoSystem/PrintReceipt.vue";
-import StudentSurveyCourse from "@/views/student/studentInfoSystem/SurveyCourse.vue";
-import StudentCheckCredit from "@/views/student/studentInfoSystem/CheckCredit.vue";
-import StudentCheckGrade from "@/views/student/studentInfoSystem/StudentCheckGrade.vue";
-import StudentCheckScholarship from "@/views/student/studentInfoSystem/StudentCheckScholarship.vue";
+import StudentStatus from "@/views/student/studentInfoSystem/status/StudentStatus.vue";
+import StudentApplyTakeOff from "@/views/student/studentInfoSystem/status/StudentApplyTakeOff.vue";
+import StudentApplyReturn from "@/views/student/studentInfoSystem/status/StudentApplyReturn.vue";
+import StudentTimelineByDepart from "@/views/student/studentInfoSystem/course/TimelineByDepart.vue";
+import StudentTimetable from "@/views/student/studentInfoSystem/course/StudentTimetable.vue";
+import StudentCheckCourse from "@/views/student/studentInfoSystem/course/CheckCourse.vue";
+import StudentPayReceipt from "@/views/student/studentInfoSystem/receipt/PayReceipt.vue";
+import StudentPrintReceipt from "@/views/student/studentInfoSystem/receipt/PrintReceipt.vue";
+import StudentSurveyCourse from "@/views/student/studentInfoSystem/survey/SurveyCourse.vue";
+import StudentCheckCredit from "@/views/student/studentInfoSystem/score/CheckCredit.vue";
+import StudentCheckGrade from "@/views/student/studentInfoSystem/score/StudentCheckGrade.vue";
+import StudentCheckScholarship from "@/views/student/studentInfoSystem/scholarship/StudentCheckScholarship.vue";
 import StudentCourseList from "@/views/student/studentInfoSystem/StudentCourseList.vue";
-import StudentInfoMain from "@/views/student/studentInfoSystem/StudentInfoMain.vue";
+import StudentInfoMain from "@/views/student/studentInfoSystem/common/StudentInfoMain.vue";
 import StudentMain from "@/views/student/studentInfoSystem/StudentSugang.vue";
 
 const routesStudent = [
