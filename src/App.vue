fa7f66e55d326771770a5dce6922f9c80cfa4d23
nalbrain <brain3918@naver.com>
2023-05-28 22:13:11 +0900

    2023-05-28

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
+  <div v-if="$route.name && !$route.name.startsWith('Member')">
     <div>
-        <EclassHeader v-if="$route.name && $route.name.startsWith('Lecture')"/>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-2">
-                    <AdminSidebar v-if="$route.name && $route.name.startsWith('Admin')"/>
-                    <StudentSidebar v-if="$route.name && $route.name.startsWith('Student')"/>
-                    <ProfessorSidebar v-if="$route.name && $route.name.startsWith('Prof')"/>
-                    <LectureSidebar v-if="$route.name && $route.name.startsWith('Lecture')"/>
-                    <EclassSidebar v-if="$route.name && $route.name.startsWith('Eclass')"/>
-                </div>
-                <div class="col-md-2">
-                </div>
-                <div class="col-md-8 position-absolute top-50 start-50 translate-middle">
-                    <router-view/>
-                </div>
-            </div>
-        </div>
+      <CommonHeader/>
+      <EclassHeader v-if="$route.name && $route.name.startsWith('Lecture')"/>
     </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-2">
+          <AdminSidebar v-if="$route.name && $route.name.startsWith('Admin')"/>
+          <StudentSidebar v-if="$route.name && $route.name.startsWith('Student')"/>
+          <ProfessorSidebar v-if="$route.name && $route.name.startsWith('Prof')"/>
+          <LectureSidebar v-if="$route.name && $route.name.startsWith('Lecture')"/>
+          <EclassSidebar v-if="$route.name && $route.name.startsWith('Eclass')"/>
+        </div>
+        <div class="col-md-10" >
+          <router-view/>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div v-if="$route.name && $route.name.startsWith('Member')">
+    <router-view/>
+  </div>
 </template>
 
 <script>
@@ -27,34 +31,28 @@ import EclassSidebar from "@/views/student/Eclass/common/EClassSidebar.vue";
 import AdminSidebar from "@/views/admin/common/AdminSidebar.vue";
 import ProfessorSidebar from "@/views/professor/common/PageSidebar.vue";
 import EclassHeader from "@/views/student/Eclass/common/PageHeader.vue";
+import CommonHeader from "@/components/common/CommonHeader.vue"
 
 export default {
-    name: 'App',
-    components: {
-        AdminSidebar,
-        EclassHeader,
-        StudentSidebar,
-        ProfessorSidebar,
-        EclassSidebar,
-        LectureSidebar,
-    }
+  name: 'App',
+  components: {
+    AdminSidebar,
+    EclassHeader,
+    CommonHeader,
+    StudentSidebar,
+    ProfessorSidebar,
+    EclassSidebar,
+    LectureSidebar
+  }
 }
 
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
-.myheader {
-    position: absolute;
-
-    left: 200px; /* 왼쪽으로 250px 이동 */
-    width: calc(100% - 250px); /* 페이지 전체 폭에서 250px를 빼서 너비를 설정 */
-    /* 다른 요소보다 위에 표시 */
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
 }
 </style>
