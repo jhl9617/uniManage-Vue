c0b18f080449041ad1a24e3ef538cefcad68e3c1
nalbrain <brain3918@naver.com>
2023-05-28 17:36:50 +0900

    2023-05-28
    자유게시판 상세보기 형태 수정

--- src/views/student/Eclass/lecture/freeboard/BoardDetail.vue
+++ src/views/student/Eclass/lecture/freeboard/BoardDetail.vue
@@ -1,79 +1,75 @@
 <template>
   <div class="board-detail">
 
-      <hr/>
-      <div class="row">
-        <div class="col-md-10">
-          <table class="table table-condensed">
-            <thead>
-            <tr align="center">
-              <th width="10%">제목</th>
-              <th width="60%">{{ free_title }}</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-              <td>작성일
-              </td>
-              <td>
-                {{ created_date }}
-              </td>
-            </tr>
-            <tr>
-              <td>글쓴이
-              </td>
-              <td>
-                {{ name }} <span style='float:right'>조회 : 1</span>
-              </td>
-            </tr>
-            <tr>
-              <td colspan="2">
-                <p>{{ free_content }}</p>
+    <hr/>
+    <div class="row">
+      <div class="col-md-10">
+        <table class="table table-condensed">
+          <thead>
+          <tr align="center">
+            <th width="10%">제목</th>
+            <th width="60%">{{ free_title }}</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>작성일
+            </td>
+            <td>
+              {{ created_date }}
+            </td>
+          </tr>
+          <tr>
+            <td>글쓴이
+            </td>
+            <td>
+              {{ name }} <span style='float:right'>조회 : 1</span>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <p>{{ free_content }}</p>
 
-              </td>
-            </tr>
-            </tbody>
-          </table>
-          <table id="commentTable" class="table table-condensed">
-            <table class="table w3-table-all">
-              <tbody>
-              <tr v-for="(row, index) in list" :key="index">
-                <td>{{ row.name }}</td>
-                <td>{{ row.free_rep_content }}</td>
-                <td>{{ row.created_date }}</td>
-                <td>
-                  <button class="w3-button w3-round w3-red" type="button" v-on:click="fnDelete(row.free_rep_id)">삭제</button>
-                </td>
-              </tr>
-              </tbody>
-            </table>
-            <hr>
-            <br><br>
-          </table>
-          <table class="table table-condensed">
-            <tr>
-              <td>
-                <span class="form-inline" role="form">
+            </td>
+          </tr>
+          </tbody>
+        </table>
+        <section class="mb-5">
+          <div class="card bg-light">
+            <div class="card-body">
+              {{ loginMember.name }} &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <button class="w3-button w3-round w3-blue-gray" type="button"
+                      v-on:click="fnSave">댓글등록
+              </button>
+              <br><br>
+              <form class="mb-4"><textarea v-model="free_rep_content" class="form-control" placeholder="댓글을 입력하세요."
+                                           rows="3"></textarea></form>
 
-                  <div class="form-group">
-                    {{ loginMember.name }}
-                  </div>
+              <div v-for="(row, index) in list" :key="index" class="d-flex mb-4">
+                <div class="flex-shrink-0"></div>
+                <div class="ms-3">
+                  <div class="fw-bold">{{ row.name }}</div>
+                  {{ row.free_rep_content }}
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
-                  <div class="form-group">
-                    <button class="w3-button w3-round w3-blue-gray" type="button"
-                            v-on:click="fnSave">댓글등록</button>&nbsp;
-                  </div>
+                </div>
 
-                  <textarea id="commentParentText" v-model="free_rep_content"
-                            class="form-control col-lg-12" rows="4" style="width:100%"></textarea>
-                </span>
-              </td>
-            </tr>
-          </table>
-          <table class="table table-condensed">
-            <thead>
-            <tr>
-              <td>
+                {{ row.created_date }}
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <button v-if="checkPermissions2(row.member_id)" class="w3-button w3-round w3-blue-gray"
+                        type="button"
+                        v-on:click="fnDelete(row.free_rep_id)">삭제
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <table class="table table-condensed">
+          <thead>
+          <tr>
+            <td>
                 <span style='float:right'>
                   <button v-if="checkPermissions()" class="w3-button w3-round w3-blue-gray"
                           type="button"
@@ -82,13 +78,13 @@
                   <button class="w3-button w3-round w3-gray" type="button"
                           v-on:click="fnList">목록</button>
                 </span>
-              </td>
-            </tr>
-            </thead>
-          </table>
-        </div>
+            </td>
+          </tr>
+          </thead>
+        </table>
       </div>
-      <hr/>
+    </div>
+    <hr/>
 
   </div>
 </template>
@@ -120,6 +116,7 @@ export default {
 
         this.free_title = res.data.freeboard.free_title
         this.member_id = res.data.freeboard.member_id
+
         this.name = res.data.freeboard.name
         this.free_content = res.data.freeboard.free_content
         this.created_date = res.data.freeboard.created_date
@@ -210,6 +207,9 @@ export default {
     },
     checkPermissions() { // 게시판 글 작성자와 로그인 한 사용자의 아이디가 일치하는지 확인
       return this.member_id === this.loginMember.member_id;
+    },
+    checkPermissions2(n) { // 게시판 글 작성자와 로그인 한 사용자의 아이디가 일치하는지 확인
+      return n === this.loginMember.member_id;
     },
   }
 }
